8e398c30c57952eed32d7f3df7782a0a984efc4f
Juan Francisco <juan.maldonado.leon@gmail.com>
2026-07-14 19:43:55 -0400

    Modelo creado

---
+++ src/models/artista.model.ts
@@ -0,0 +1,13 @@
+export class Artista {
+    id?: number;
+    nombre: string;
+    biografia:string;
+    pais:string;
+    generoMusical:string[];
+    oyentesMensuales:number;
+    tipo:string;
+    verificado:boolean;
+
+  constructor(
+  ) {}
+}
