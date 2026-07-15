94e90adc1d8c0f922c4d2dcde79a8cf287a84e05
Juan Francisco <juan.maldonado.leon@gmail.com>
2026-07-14 20:59:17 -0400

    Agrego cors & nuevos controladores & endpoint para listar artistas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlaylistsController } from './playlists/playlists.controller';
+import { CancionesController } from './canciones/canciones.controller';
+import { ArtistasController } from './artistas/artistas.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PlaylistsController, CancionesController, ArtistasController],
   providers: [AppService],
 })
 export class AppModule {}
